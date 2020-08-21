605846ef25a40501c78dbd5740a2772acbb385e9
fpavogt <fpavogt@users.noreply.github.com>
2020-08-21 16:33:38 +0200

    Prepare the docs area.

---
+++ docs/build_docs.sh
@@ -0,0 +1,17 @@
+# Custom bash script to generate the mannkendall docs
+# Created August 2020; F.P.A. Vogt; frederic.vogt@meteoswiss.ch
+#
+
+# Step 0: just clean the existing apidoc rst files and any previous build folder
+rm -rf ./source/modules
+rm -rf ./build
+
+# Step 1: run autodoc to generate all the docstring rst files.
+# Force the rewrite of all of them to capture *all* the recent changes.
+sphinx-apidoc -f -M -o ./source/modules/ ../src/
+
+# Delete the superfluous module.rst file that the previous command creates.
+rm -f ./source/modules/modules.rst
+
+# Generate the documentation, storing it in the build directory
+sphinx-build -a -b html ./source ./build
