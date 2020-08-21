f67878614eb5ce880c4d7535f2a9524414eefc28
fpavogt <fpavogt@users.noreply.github.com>
2020-08-21 17:05:14 +0200

    Setup package structure

---
+++ setup.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright (c) 2020 MeteoSwiss, contributors listed in AUTHORS.
+
+Distributed under the terms of the BSD 3-Clause License.
+
+SPDX-License-Identifier: BSD-3-Clause
+"""
+
+from pathlib import Path
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+
+# Get the VERSION from the dedicate file
+with open(Path('.') / 'src' / 'mk_version.py') as fid:
+    version = next(
+        line.split("'")[1] for line in fid.readlines() if 'VERSION' in line
+    )
+
+# Extract the long description from the README
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    dependency_links=[],
+    name="mannkendall",
+    version=version,
+    license='BSD 3-Clause',
+
+    # Include all packages under src
+    packages=find_packages("src"),
+
+    # Tell setuptools packages are under src
+    package_dir={"": "src"},
+
+    url="https://github.com/MeteoSwiss-MDA/mannkendall",
+    author="MeteoSwiss",
+    author_email="fpavogt@users.noreply.github.com",
+    description="TBD",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires='>=3.8.0',
+    install_requires=[],
+
+    classifiers=[
+
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Meteorology',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: BSD License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.8',
+
+    ],
+
+    include_package_data=False,  # If True, non .py files make it onto pypi !
+
+)
---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright (c) 2020 MeteoSwiss, contributors listed in AUTHORS.
+
+Distributed under the terms of the BSD 3-Clause License.
+
+SPDX-License-Identifier: BSD-3-Clause
+"""
+
+# Get the version of the code to behave as usual
+from mk_version import VERSION as __version__
