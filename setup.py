e1213fe137f16796304d8ec23e82ea8ef9ea2916
fpavogt <fpavogt@users.noreply.github.com>
2020-08-24 15:00:56 +0200

    Add pytest to requirements

--- setup.py
+++ setup.py
@@ -37,7 +37,8 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     python_requires='>=3.8.0',
-    install_requires=["numpy >= 1.19"],
+    install_requires=["numpy >= 1.19",
+                      "pytest"],
 
     classifiers=[
 
